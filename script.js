5d1e363226b8ed8ea9182746ec910d88f6055ec2
Kieran Colaco <kieran.colaco@yahoo.com>
2020-10-15 23:27:06 -0400

    Finished
    Website is ready to go live.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
+var getNavID = document.getElementById("nav-id");
 
 hamburger.addEventListener('click',()=>{
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+    
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+	getNavID.style.width="100vw";
+	
+	
 });
-
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
@@ -16,3 +20,17 @@ document.addEventListener('scroll', () => {
 		header.style.backgroundColor = 'transparent';
 	}
 });
+
+var lis= document.getElementById("nav-id").getElementsByTagName('li');
+
+for(var i=0; i<lis.length;i++){
+	lis[i].addEventListener('click', hideNavBar, false);
+}
+
+function hideNavBar(){
+	
+	getNavID.style.width="0";
+	hamburger.classList.toggle('active',false);
+	mobile_menu.classList.toggle('active', false);
+	
+}
